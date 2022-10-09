9b9f0f35572f1313628fabe1da2e73c63f294034
ardamoin <ardamoin2@gmail.com>
2022-10-09 14:06:56 -0400

    Make function to get random computer choice for rock papaer scissors

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 function randomIndex() {
     return Math.floor(Math.random() * 3);
 }
+
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    return choices[randomIndex()];
+}
