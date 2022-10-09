6b0b8dbe350a80b8e67930be269a84f3dca4e21b
ardamoin <ardamoin2@gmail.com>
2022-10-09 14:27:37 -0400

    Make function to play a single round in app.js

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ function randomIndex() {
 }
 
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
     return choices[randomIndex()];
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    let player = playerSelection.toLowerCase();
+    let computer =   computerSelection.toLowerCase();
+
+    if (player === computer) {
+        return "Draw";
+    }
+
+    if ((player === "rock" && computer === "scissors") || (player === "paper" && computer === "rock") || (player === "scissors" && computer === "paper")){
+        return "You Win! " + player[0].toUpperCase() + " beats " + computer[0].toUpperCase(); 
+    } else {
+        return "You Lose! " + computer[0].toUpperCase() + " beats " + player[0].toUpperCase();
+    }
 }
